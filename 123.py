a908a09471c51ced95dac09cf4d5bd52c71fd9c4
jweiban <bangguo.wei@jci.com>
2022-06-24 13:54:12 +0800

    第二次

--- 123.py
+++ 123.py
@@ -1,1 +1,21 @@
-print(“hello”)
+import lifelines
+import matplotlib.pyplot as plt
+from lifelines.datasets import load_waltons
+from lifelines import KaplanMeierFitter
+
+df = load_waltons()  # returns a Pandas DataFrame
+
+T = df['T']
+E = df['E']
+
+kmf = KaplanMeierFitter()
+kmf.fit(T, event_observed=E)  # more succiently, kmf.fit(T,E)
+
+kmf.survival_function_
+
+kmf.median_survival_time_
+
+kmf.plot()
+plt.show()
+
+#%%
