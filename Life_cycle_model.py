9dca91cf65a7529accb1f611ea8eeeef08b2366a
jweiban <bangguo.wei@jci.com>
2022-06-24 14:17:53 +0800

    Third commit

--- Life_cycle_model.py
+++ Life_cycle_model.py
@@ -1,8 +1,11 @@
 import lifelines
 import matplotlib.pyplot as plt
-from lifelines.datasets import load_waltons
+from lifelines.datasets import load_waltons,load_rossi
 from lifelines import KaplanMeierFitter
+from lifelines import CoxPHFitter
 
+#%%
+#KaplanMeierFitter Moedl
 df = load_waltons()  # returns a Pandas DataFrame
 
 T = df['T']
@@ -12,10 +15,16 @@ kmf = KaplanMeierFitter()
 kmf.fit(T, event_observed=E)  # more succiently, kmf.fit(T,E)
 
 kmf.survival_function_
-
 kmf.median_survival_time_
 
 kmf.plot()
 plt.show()
 
 #%%
+#Cox Model
+rossi = load_rossi()
+rossi.head()
+cph = CoxPHFitter()
+cph.fit(rossi, duration_col='week', event_col='arrest')
+cph.plot()
+plt.show()
